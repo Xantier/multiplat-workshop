84e7c8bef665f949482fab5a5b7166f7d94161c4
Jussi Hallila <jussi@hallila.com>
2018-03-06 21:15:36 +0000

    Modifying modules, consolidating build files, modifying frontend packaging and adding webpack dev server

---
+++ multiplat-workshop-common/src/main/kotlin/api/Api.kt
@@ -0,0 +1,6 @@
+package api
+expect class ApiClient{
+    suspend fun fetchPhotos(): String
+}
+
+const val PHOTO_URL = "https://api.meetup.com/Dublin-Kotliners/events/243009138/photos?&sign=true&photo-host=public&page=20&photo-host=public&page=20"
