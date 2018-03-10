fe0c76ccb29e425e6e643921f44822ca25de619a
Jussi Hallila <jussi@hallila.com>
2018-03-10 11:50:08 +0000

    Removing unneeded extension function, Kotlin coroutines-core-js provides that to us already

--- multiplat-workshop-js/src/main/kotlin/api/ApiClient.kt
+++ multiplat-workshop-js/src/main/kotlin/api/ApiClient.kt
@@ -1,17 +1,16 @@
 package api
 
+import kotlinx.coroutines.experimental.await
 import kotlinx.coroutines.experimental.launch
 import org.w3c.fetch.RequestInit
 import kotlin.browser.window
-import kotlin.coroutines.experimental.suspendCoroutine
-import kotlin.js.Promise
 import kotlin.js.json
 
 const val CORS_ANYWHERE = "https://cors-anywhere.herokuapp.com/"
 
 actual class ApiClient {
     actual fun fetchPhotos(): String {
-        launch{
+        launch {
             val response = window.fetch(CORS_ANYWHERE + PHOTO_URL, object : RequestInit {
                 override var headers: dynamic = json(
                     "Accept" to "application/json",
@@ -22,10 +21,4 @@ actual class ApiClient {
         }
         return "null"
     }
-}
-
-suspend fun <T> Promise<T>.await() = suspendCoroutine<T> { cont ->
-    then({ value -> cont.resume(value) },
-        { exception -> cont.resumeWithException(exception) }
-    )
 }
